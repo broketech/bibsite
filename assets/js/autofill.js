0b5e613aa67e052eaf45c5e75e3a32a08d7be044
broketech <broketech@randomactual.space>
2020-02-16 10:54:13 -0800

    dump autocomplete list before next run

--- assets/js/autofill.js
+++ assets/js/autofill.js
@@ -9,11 +9,14 @@ function autofillRequest(ID){
   autofillClient.get(bibapiUrl + 'predict/'  + inputText, function(response) {
     console.log('response ' + typeof response)
     let listName = document.getElementById(IDstring + '-datalist');
+    let child;
+    while (child = listName.firstChild) {
+      child.remove();
+    }
     for(let x of response){
       let option = document.createElement('option');
       option.value = x;
       listName.appendChild(option);
-      console.log(x)
     }
   });
 }
